e49a065f184136ab474739d89c91a870d19c31b1
HaneefahAliu <aliuhaneefah@gmail.com>
2020-11-14 17:09:26 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, {useState} from "react";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
+import {nanoid} from "nanoid";
+
+const FILTER_MAP = {
+    All: () => true,
+    Active: task => !task.completed,
+    Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+    const [tasks, setTasks] = useState(props.tasks);
+    const [filter, setFilter] = useState('All');
+
+    function addTask(name) {
+        const newTask = {
+            id: "todo-" + nanoid(),
+            name: name,
+            completed: false
+        };
+        setTasks([
+            ...tasks,
+            newTask
+        ]);
+    }
+
+    function toggleTaskCompleted(id) {
+        const updatedTasks = tasks.map(task => {
+            // if this task has the same ID as the edited task
+            if (id === task.id) {
+                // use object spread to make a new object whose `completed` prop has been
+                // inverted
+                return {
+                    ...task,
+                    completed: !task.completed
+                }
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+    }
+
+    function deleteTask(id) {
+        const remainingTasks = tasks.filter(task => id !== task.id);
+        setTasks(remainingTasks);
+    }
+
+    const taskList = tasks
+        .filter(FILTER_MAP[filter])
+        .map(task => (
+            <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} toggleTaskCompleted={toggleTaskCompleted} 
+            // deleteTask={deleteTask}
+                // editTask={editTask}
+/>
+        ));
+
+    const filterList = FILTER_NAMES.map(name => (
+        <FilterButton
+            key={name}
+            name={name}
+            isPressed={name === filter}
+            setFilter={setFilter}/>
+    ));
+
+    return (
+        <div className="todoapp stack-large">
+            <Form addTask={addTask}/>
+            <div className="filters btn-group stack-exception">
+                {filterList}
+            </div>
+            <ul >
+                {taskList}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+
+function Form(props) {
+    const [name, setName] = useState('');
+
+    function handleChange(e) {
+        setName(e.target.value);
+      }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.addTask(name);
+        setName("");
+      }
+
+    return (
+        <form onSubmit={handleSubmit}>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            #todo
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="add-input input input__lg"
+          placeholder="add details"
+          name="text"
+          autoComplete="off"
+          value={name}
+          onChange={handleChange}
+        />
+        <button type="submit" className="add-btn btn__primary btn__lg">
+          Add
+        </button>
+      </form>
+    );
+}
+
+export default Form;
